cd7085f9d0193071acaa7ca244b113bd87d5b674
LetiTran <leticiastran@gmail.com>
2018-05-22 16:21:31 -0700

    making a post request with dee

--- index.js
+++ index.js
@@ -1,36 +1,104 @@
-const URL = 'http://petdibs.herokuapp.com/pets';
+const URL = 'https://petdibs.herokuapp.com/pets';
 
+//
+// Status Management
+//
 const reportStatus = (message) => {
   $('#status-message').html(message);
-}
+};
 
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
+//
+// Loading Pets
+//
 const loadPets = () => {
+  reportStatus('Loading pets...');
+
   const petList = $('#pet-list');
   petList.empty();
 
-// Call on load status:
-reportStatus('Loading Pets! Please wait...');
-
-  // get the thing:
-  axios.get(URL) //returns a promisse (newest way to handle a asyncronist thing in JS)
+  axios.get(URL)
   .then((response) => {
-    console.log('This is the .then');
-    //  get the data from the response and append li's to the DOM:
+    reportStatus(`Successfully loaded ${response.data.length} pets`);
     response.data.forEach((pet) => {
       petList.append(`<li>${pet.name}</li>`);
     });
-    reportStatus('Pets Loaded!');
   })
-  // if API fails:
   .catch((error) => {
+    reportStatus(`Encountered an error while loading pets: ${error.message}`);
     console.log(error);
-    reportStatus(`Error: ${error.message}`);
+  });
+};
+
+//
+// Creating Pets
+//
+const FORM_FIELDS = ['name', 'age', 'owner'];
+const inputField = name => $(`#pet-form input[name="${name}"]`);
+
+const readFormData = () => {
+  const getInput = name => {
+    const input = inputField(name).val();
+    return input ? input : undefined;
+  };
+
+  const formData = {};
+  FORM_FIELDS.forEach((field) => {
+    formData[field] = getInput(field);
   });
 
-  console.log('This is after .get');
+  return formData;
+};
+
+const clearForm = () => {
+  FORM_FIELDS.forEach((field) => {
+    inputField(field).val('');
+  });
 }
 
-// Load Pets when the user clicks on the button:
+const createPet = (event) => {
+  // Note that createPet is a handler for a `submit`
+  // event, which means we need to call `preventDefault`
+  // to avoid a page reload
+  event.preventDefault();
+
+  const petData = readFormData();
+  console.log(petData);
+
+  reportStatus('Sending pet data...');
+
+  axios.post(URL, petData)
+  .then((response) => {
+    reportStatus(`Successfully added a pet with ID ${response.data.id}!`);
+    clearForm();
+  })
+  .catch((error) => {
+    console.log(error.response);
+    if (error.response.data && error.response.data.errors) {
+      reportError(
+        `Encountered an error: ${error.message}`,
+        error.response.data.errors
+      );
+    } else {
+      reportStatus(`Encountered an error: ${error.message}`);
+    }
+  });
+};
+
+//
+// OK GO!!!!!
+//
 $(document).ready(() => {
   $('#load').click(loadPets);
-})
+  $('#pet-form').submit(createPet); // $('#pet-form').on('submit', createPet);
+});
