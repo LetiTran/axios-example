fa90e9c6e17e6cb71ab125c01ce7aa2636ba1e9b
LetiTran <leticiastran@gmail.com>
2018-05-22 10:47:46 -0700

    Worked on code with Chris during class.

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-/* Code goes here */
+const URL = 'http://petdibs.herokuapp.com/pets';
 
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+}
+
+const loadPets = () => {
+  const petList = $('#pet-list');
+  petList.empty();
+
+// Call on load status:
+reportStatus('Loading Pets! Please wait...');
+
+  // get the thing:
+  axios.get(URL) //returns a promisse (newest way to handle a asyncronist thing in JS)
+  .then((response) => {
+    console.log('This is the .then');
+    //  get the data from the response and append li's to the DOM:
+    response.data.forEach((pet) => {
+      petList.append(`<li>${pet.name}</li>`);
+    });
+    reportStatus('Pets Loaded!');
+  })
+  // if API fails:
+  .catch((error) => {
+    console.log(error);
+    reportStatus(`Error: ${error.message}`);
+  });
+
+  console.log('This is after .get');
+}
+
+// Load Pets when the user clicks on the button:
+$(document).ready(() => {
+  $('#load').click(loadPets);
+})
